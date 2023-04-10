eaf4dba6f629ceb39a17aaa6c74a972e76d34671
Leonardo Vargas <lvbernal@gmail.com>
2023-04-09 23:05:12 -0500

    build models from api results

--- python/vqf/models.py
+++ python/vqf/models.py
@@ -1,34 +1,72 @@
 class Antenna:
 
-    def __init__(self, azimuth: float, tilt: float, power: float):
+    def __init__(self, id: str, name: str, azimuth: float, tilt: float, power: float):
+        self.id: str = id
+        self.name: str = name
         self.azimuth: float = azimuth
         self.tilt: float = tilt
         self.power: float = power
 
+    def __str__(self):
+        return f"{self.name} (AZ {self.azimuth}, TL {self.tilt}, PW: {self.power})"
+
+    @classmethod
+    def from_api_dict(cls, j) -> "Antenna":
+        return Antenna(j["id"], j["name"], j["azimuth"], j["tilt"], j["power"])
+
 
 class Site:
 
-    def __init__(self, id: str, latitude: str, longitude: str, user_id: str):
-        self.id: str = id
-        self.latitude: str = latitude
-        self.longitude: str = longitude
-        self.user_id: str = user_id
+    def __init__(self, latitude: float, longitude: float):
+        self.latitude: float = latitude
+        self.longitude: float = longitude
         self.antennas: list[Antenna] = []
+
+    def __str__(self):
+        return f"Lat: {self.latitude}, Lon: {self.longitude}"
+
+    @classmethod
+    def from_api_dict(cls, j) -> list["Site"]:
+        unique_locations = set(
+            [(t["location"]["latitude"], t["location"]["longitude"]) for t in j])
+        sites = []
+        for l in unique_locations:
+            site = Site(l[0], l[1])
+            antennas = filter(
+                lambda s: s["location"]["latitude"] == site.latitude and
+                s["location"]["longitude"] == site.longitude,
+                j)
+            site.antennas = [Antenna.from_api_dict(a) for a in antennas]
+            sites.append(site)
+        return sites
 
 
 class Project:
 
-    def __init__(self, id: int, name: str, avg_receiver_height: float, propagation_model: str, threshold: float, simulated: bool):
+    def __init__(
+            self, id: int, name: str, avg_receiver_height: float,
+            propagation_model: str, threshold: float, simulated: bool):
         self.id: int = id
         self.name: str = name
         self.avg_receiver_height = avg_receiver_height
         self.propagation_model: str = propagation_model
         self.threshold: float = threshold
         self.simulated: bool = simulated
+        self.sites: list["Site"] = []
 
     def __str__(self):
         return f"{self.id} {self.name} ({self.propagation_model}, THR {self.threshold}, ARH {self.avg_receiver_height})"
 
     @classmethod
     def from_api_dict(cls, j) -> "Project":
-        return Project(j["id"], j["name"], j["averageReceiverHeight"], j["propagationModel"], j["threshold"], j["existsSimulation"])
+        return Project(
+            j["id"], j["name"], j["averageReceiverHeight"],
+            j["propagationModel"], j["threshold"], j["existsSimulation"]
+        )
+
+    @classmethod
+    def from_api_dict_detailed(cls, j) -> "Project":
+        project = cls.from_api_dict(j)
+        transmitters = j["transmitterList"]
+        project.sites = Site.from_api_dict(transmitters)
+        return project
