07f5ba7b03f08d6b4bef1e6600639eed008010ca
Leonardo Vargas <lvbernal@gmail.com>
2023-04-09 22:13:32 -0500

    list projects

--- python/vqf/celgis.py
+++ python/vqf/celgis.py
@@ -6,6 +6,7 @@ class Celgis:
     def __init__(self, api_url: str, username: str, password: str):
         self.api_url = api_url
         self.token: str = self._signin(username, password)
+        self.headers: dict = {"Authorization": f"Bearer {self.token}"}
 
     def _signin(self, username, password) -> str:
         url = f"{self.api_url}/users/sign-in"
@@ -16,3 +17,8 @@ class Celgis:
 
     def is_active(self) -> bool:
         return self.token is not None
+
+    def list_projects(self):
+        url = f"{self.api_url}/projects"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
