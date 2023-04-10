10f3540e237189f277f0bb4a9136f48bae1380e3
Leonardo Vargas <lvbernal@gmail.com>
2023-04-09 21:27:01 -0500

    vqf sample

--- python/vqf/vqf.py
+++ python/vqf/vqf.py
@@ -1,6 +1,15 @@
-import logging
+##############################################################
+# Grupo i2t. Universidad Icesi                               #
+# Módulo Python para interactura con el                      #
+# modelo de optimización ODISEO                              #
+##############################################################
 
+#################### Configurar logs ####################
+
+import logging
 logging.basicConfig(filename="vqf_logs.log", level=logging.DEBUG)
+
+#################### Conexión del modelo ####################
 
 from minizinc import Instance, Model, Solver
 
@@ -17,13 +26,13 @@ instance["M"] = 5
 # Distancia entre la i-ésima estación base y el k-ésimo punto de medición. float
 instance["d_ik"] = [[1.5, 2.0, 3.0, 4.3, 5.1], [5.4, 4.0, 3.1, 2.9, 1.8]]
 # Población que demanda servicio en el punto k. int
-instance["pob_k"] = [100, 90, 20, 50, 80]
+instance["pob_k"] = [100, 100, 100, 100, 100]
 # Indica si dos antenas comparten el mismo Bloque de Recursos. j {1..3N}. bool
 instance["cocan_ij"] = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 # Cantidad recursos asignados a la radiobase i. int
 instance["channels"] = [20, 20]
 # Número de Sectores disponibles para prestar el servicio. int
-instance["maxSol"] = 8
+instance["maxSol"] = 6
 # Cardinalidad mínima deseada para el active set, se busca que en la mayoría 
 # de puntos de medición la superen sin que no hacerlo represente infactibilidad. 
 # Corresponde al número mínimo de sectores que deberían poder atender el servicio. int
@@ -42,22 +51,24 @@ instance["W2"] = 1
 
 #################### Estructuras de datos ####################
 
+# Estas estructuras de datos se construyen a partir de los resultados de Celgis
+
 # Flag que indica si el punto k tiene cobertura de alguna radiobase. bool
 instance["C_k"] = [1, 0, 0, 1, 0]
 # Indica si la radiobase i cubre el punto k
-instance["lRbs_ik"] = [[1, 1, 1, 1, 1], [1, 0, 0, 1, 0]]
+instance["lRbs_ik"] = [[0, 0, 0, 1, 0], [1, 0, 0, 1, 0]]
 # Si lRbs_ik, indica el índice h de la antena que lo cubre
-instance["lAnt_ik"] = [[1, 1, 2, 3, 2], [3, 1, 2, 3, 1]]
+instance["lAnt_ik"] = [[0, 0, 0, 2, 0], [3, 0, 0, 2, 0]]
 # Nivel de señal que desde la antena lAnt_ik llega al punto
-instance["lSig_ik"] = [[1.5, 2.0, 3.0, 4.3, 5.1], [5.4, 4.0, 3.1, 2.9, 1.8]]
+instance["lSig_ik"] = [[-100.0, -100.0, -100.0, -50.0, -100.0], [-20.0, -100.0, -100.0, -80.0, -100.0]]
 # Indica si lSig_ik supera el umbral de cobertura
-instance["lFlgCob_ik"] = [[0, 1, 1, 0, 1], [1, 0, 0, 1, 0]]
+instance["lFlgCob_ik"] = [[0, 0, 0, 1, 0], [1, 0, 0, 0, 0]]
 # Ángulo de azimut con el que la antena lAnt_ik incide en el punto k
-instance["lAz_ik"] = [[1.5, 2.0, 3.0, 4.3, 5.1], [5.4, 4.0, 3.1, 2.9, 1.8]]
+instance["lAz_ik"] = [[0.0, 0.0, 0.0, 30.0, 0.0], [90.0, 0.0, 0.0, 300.0, 0.0]]
 # Ángulo de elevación con el que la antena lAnt_ik incide en el punto k
-instance["lEl_ik"] = [[1.5, 2.0, 3.0, 4.3, 5.1], [5.4, 4.0, 3.1, 2.9, 1.8]]
+instance["lEl_ik"] = [[3.0, 3.0, 3.0, 3.0, 3.0], [3.0, 3.0, 3.0, 3.0, 3.0]]
 # Índice de la radiobase que brinda el nivel de señal más alto en el punto k
-instance["indDesSig_k"] = [1, 2, 1, 2, 1]
+instance["indDesSig_k"] = [1, 0, 0, 2, 0]
 
 
 result = instance.solve()
