03ee6d03f3202d724bfeb5aae92ad883079d51e5
Leonardo Vargas <lvbernal@gmail.com>
2023-04-09 22:13:44 -0500

    improved project model

--- python/vqf/models.py
+++ python/vqf/models.py
@@ -18,11 +18,17 @@ class Site:
 
 class Project:
 
-    def __init__(self, id: int, name: str, avg_receiver_height: float, propagation_model: str, threshold: float, service: str, sites: list[Site]):
+    def __init__(self, id: int, name: str, avg_receiver_height: float, propagation_model: str, threshold: float, simulated: bool):
         self.id: int = id
         self.name: str = name
         self.avg_receiver_height = avg_receiver_height
         self.propagation_model: str = propagation_model
         self.threshold: float = threshold
-        self.service: str = service
-        self.sites: list[Site] = sites
+        self.simulated: bool = simulated
+
+    def __str__(self):
+        return f"{self.id} {self.name} ({self.propagation_model}, THR {self.threshold}, ARH {self.avg_receiver_height})"
+
+    @classmethod
+    def from_api_dict(cls, j) -> "Project":
+        return Project(j["id"], j["name"], j["averageReceiverHeight"], j["propagationModel"], j["threshold"], j["existsSimulation"])
