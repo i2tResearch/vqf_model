d1e4af8a2050c7f216053c9c3c3586833b49a4c7
Leonardo Vargas <lvbernal@gmail.com>
2023-04-09 23:05:24 -0500

    cli ui

--- python/vqf/run.py
+++ python/vqf/run.py
@@ -3,13 +3,6 @@ from celgis import Celgis
 from models import Project
 
 
-def build_project_list(projects) -> list:
-    projects_list: list = []
-    for i, p in enumerate(projects):
-        projects_list.append((i, Project.from_api_dict(p)))
-    return projects_list
-
-
 @click.command()
 @click.option("--api_url", default="http://172.28.16.1:8080/api", help="Celgis Url")
 @click.option("--username", help="Celgis username")
@@ -17,21 +10,35 @@ def build_project_list(projects) -> list:
 def run_vqf(api_url, username, password):
     print("=================== ODISEO/VQ ===================")
     print("Using API endpoint", api_url)
+
     celgis = Celgis(api_url, username, password)
     if (not celgis.is_active()):
-        print("Could not activate celgis connector")
+        print("Could not activate celgis connector. Check api_url and credentials")
         return
+    
     print("Celgis connector is active")
     print("=================================================")
     print("Available projects:")
+
     json_projects = celgis.list_projects()
-    projects = build_project_list(json_projects)
-    for p in projects:
-        print("\t", p[0], str(p[1]))
+    projects = [Project.from_api_dict(j) for j in json_projects]
+    for i, p in enumerate(projects):
+        print("\t", i, p)
+
     print("=================================================")
     index = click.prompt("Enter the project index", type=int, default=0)
-    project = projects[index][1]
-    print("Selected project:", project.id)
+    selected = projects[index]
+    print("Selected project:", projects[index].id)
+
+    json_project = celgis.get_project(selected.id)
+    project = Project.from_api_dict_detailed(json_project)
+    
+    print("=================================================")
+    print("Project", project)
+    for s in project.sites:
+        print("\t", "Site", s)
+        for a in s.antennas:
+            print("\t", "\t", "Antenna", a)
 
 
 if __name__ == '__main__':
